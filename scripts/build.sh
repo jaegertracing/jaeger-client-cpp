6c0cc7c1c4b8f8b9776d4fb523cf0cde92da36ef
Isaac Hier <ihier@uber.com>
2018-03-26 13:18:50 -0400

    Use set -e
    Signed-off-by: Isaac Hier <ihier@uber.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,12 @@
 
 # Based on https://github.com/codecov/example-cpp11-cmake/blob/master/run_build.sh.
 
+set -e
+
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 NO_COLOR='\033[0m'
 GREEN='\033[0;32m'
-
-function error() {
-    >&2 echo -e "${RED}$1${NO_COLOR}"
-}
 
 function info() {
     echo -e "${GREEN}$1${NO_COLOR}"
@@ -19,27 +17,15 @@ function working() {
     echo -e "${BLUE}$1${NO_COLOR}"
 }
 
-mkdir -p build
-cd build || exit
-if [ "x$COVERAGE" != "x" ]; then
-  cmake -DCMAKE_BUILD_TYPE=Debug -DJAEGERTRACING_COVERAGE=ON ..
-else
-  cmake -DCMAKE_BUILD_TYPE=Debug -DJAEGERTRACING_COVERAGE=OFF ..
-fi
+function main() {
+    mkdir -p build
+    cd build
+    coverage_option=${COVERAGE:+"-DJAEGERTRACING_COVERAGE=ON"}
+    cmake -DCMAKE_BUILD_TYPE=Debug "${coverage_option}" ..
+    make -j3 UnitTest
+    info "Running tests..."
+    ./UnitTest
+    working "All tests compiled and passed"
+}
 
-if make -j3 UnitTest; then
-    true
-else
-    error "Error: compilation errors"
-    exit 1
-fi
-
-info "Running tests..."
-if ./UnitTest; then
-    true
-else
-    error "Error: test failure"
-    exit 1
-fi
-
-working "All tests compiled and passed"
+main
