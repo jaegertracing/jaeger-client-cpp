1b8ed62bbba10927ee29050426c3efe2e2ff9674
Ankit Varma <ankit409@gmail.com>
2018-04-16 12:44:27 -0400

    (bugfix) Unhandled exception - Abort raised when write to the jaeger-… (#80)
    * (bugfix) Unhandled exception - Abort raised when write to the jaeger-agent fails
    
    Signed-off-by: ankit.varma10 <ankit.varma.ub@gmail.com>
    
    * (bugfix) review comments - application crashing when agent unavailable
    
    Signed-off-by: ankit.varma10 <ankit.varma.ub@gmail.com>
    
    * Add test case for new exception handling code
    
    Signed-off-by: Isaac Hier <isaachier@gmail.com>
    
    * Add coverage for unknown exceptions
    
    Signed-off-by: Isaac Hier <isaachier@gmail.com>

--- src/jaegertracing/UDPTransport.cpp
+++ src/jaegertracing/UDPTransport.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Uber Technologies, Inc.
+ * Copyright (c) 2017-2018 Uber Technologies, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -113,7 +113,22 @@ int UDPTransport::flush()
     thrift::Batch batch;
     batch.__set_process(_process);
     batch.__set_spans(_spanBuffer);
-    _client->emitBatch(batch);
+
+    try {
+        _client->emitBatch(batch);
+    } catch (const std::system_error& ex) {
+        std::ostringstream oss;
+        oss << "Could not send span " << ex.what()
+            << ", code=" << ex.code().value();
+        throw Transport::Exception(oss.str(), _spanBuffer.size());
+    } catch (const std::exception& ex) {
+        std::ostringstream oss;
+        oss << "Could not send span " << ex.what();
+        throw Transport::Exception(oss.str(), _spanBuffer.size());
+    } catch (...) {
+        throw Transport::Exception("Could not send span, unknown error",
+                                   _spanBuffer.size());
+    }
 
     resetBuffers();
 
--- src/jaegertracing/UDPTransportTest.cpp
+++ src/jaegertracing/UDPTransportTest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Uber Technologies, Inc.
+ * Copyright (c) 2017-2018 Uber Technologies, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,50 @@
 #include "jaegertracing/utils/ErrorUtil.h"
 
 namespace jaegertracing {
+namespace {
+
+class MockUDPClient : public utils::UDPClient {
+  public:
+    enum class ExceptionType { kSystemError, kException, kString };
+
+    MockUDPClient(const net::IPAddress& serverAddr,
+                  int maxPacketSize,
+                  ExceptionType type)
+        : UDPClient(serverAddr, maxPacketSize)
+        , _type(type)
+    {
+    }
+
+  private:
+    void emitBatch(const thrift::Batch& batch) override
+    {
+        switch (_type) {
+        case ExceptionType::kSystemError:
+            throw std::system_error();
+        case ExceptionType::kException:
+            throw std::exception();
+        default:
+            assert(_type == ExceptionType::kString);
+            throw "error";
+        }
+    }
+
+    ExceptionType _type;
+};
+
+class MockUDPTransport : public UDPTransport {
+  public:
+    MockUDPTransport(const net::IPAddress& ip,
+                     int maxPacketSize,
+                     MockUDPClient::ExceptionType type)
+        : UDPTransport(ip, maxPacketSize)
+    {
+        setClient(std::unique_ptr<utils::UDPClient>(
+            new MockUDPClient(ip, maxPacketSize, type)));
+    }
+};
+
+}  // anonymous namespace
 
 TEST(UDPTransport, testManyMessages)
 {
@@ -37,6 +81,27 @@ TEST(UDPTransport, testManyMessages)
         Span span(tracer);
         span.SetOperationName("test" + std::to_string(i));
         ASSERT_NO_THROW(sender.append(span));
+    }
+}
+
+TEST(UDPTransport, testExceptions)
+{
+    const auto handle = testutils::TracerUtil::installGlobalTracer();
+    const auto tracer =
+        std::static_pointer_cast<const Tracer>(opentracing::Tracer::Global());
+
+    Span span(tracer);
+    span.SetOperationName("test");
+
+    const MockUDPClient::ExceptionType exceptionTypes[] = {
+        MockUDPClient::ExceptionType::kSystemError,
+        MockUDPClient::ExceptionType::kException,
+        MockUDPClient::ExceptionType::kString
+    };
+    for (auto type : exceptionTypes) {
+        MockUDPTransport sender(net::IPAddress(), 0, type);
+        sender.append(span);
+        ASSERT_THROW(sender.flush(), Transport::Exception);
     }
 }
 
