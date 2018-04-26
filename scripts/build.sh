359979c39381d537bf1341f382cb7fa0b44630c0
isaachier <isaachier@gmail.com>
2018-04-26 16:39:18 -0400

    Add plugin build (#82)
    Signed-off-by: Isaac Hier <isaachier@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,10 +18,14 @@ function working() {
 }
 
 function main() {
+    local project_dir
+    project_dir=$(git rev-parse --show-toplevel)
+    cd "$project_dir"
+
     mkdir -p build
     cd build
     coverage_option=${COVERAGE:+"-DJAEGERTRACING_COVERAGE=ON"}
-    cmake -DCMAKE_BUILD_TYPE=Debug "${coverage_option}" ..
+    cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON "${coverage_option}" ..
     make -j3 UnitTest
     info "Running tests..."
     ./UnitTest
--- src/jaegertracing/DynamicLoadTest.cpp
+++ src/jaegertracing/DynamicLoadTest.cpp
@@ -21,6 +21,44 @@
 #include <opentracing/dynamic_load.h>
 
 namespace jaegertracing {
+namespace {
+
+void trace(opentracing::TracerFactory& factory)
+{
+    std::string errorMessage;
+    auto result = factory.MakeTracer(R"(
+service_name: test-service
+disabled: false
+sampler:
+    type: probabilistic
+    param: 0.001
+reporter:
+    queueSize: 100
+    bufferFlushInterval: 10
+    logSpans: false
+    localAgentHostPort: 127.0.0.1:6831
+headers:
+    jaegerDebugHeader: debug-id
+    jaegerBaggageHeader: baggage
+    TraceContextHeaderName: trace-id
+    traceBaggageHeaderPrefix: testctx-
+baggage_restrictions:
+    denyBaggageOnInitializationFailure: false
+    hostPort: 127.0.0.1:5778
+    refreshInterval: 60
+)",
+                                     errorMessage);
+    assert(errorMessage.empty());
+    auto tracer = static_cast<std::shared_ptr<opentracing::Tracer>>(*result);
+    {
+        tracer->StartSpan("a");
+        std::cout << "Started span\n";
+    }
+    std::cout << "Finished span\n";
+}
+
+}  // anonymous namespace
+
 #ifdef JAEGERTRACING_WITH_YAML_CPP
 TEST(DynamicLoad, invalidVersion)
 {
@@ -44,7 +82,9 @@ TEST(DynamicLoad, validVersion)
     ASSERT_EQ(rcode, 0);
     ASSERT_EQ(errorCategory, nullptr);
     ASSERT_NE(tracerFactory, nullptr);
-    delete static_cast<opentracing::TracerFactory*>(tracerFactory);
+    std::unique_ptr<opentracing::TracerFactory> factory(
+        static_cast<opentracing::TracerFactory*>(tracerFactory));
+    trace(*factory);
 }
 #else
 TEST(DynamicLoad, noYAML)
