eec64df50c2960e8ececa7e84f54337c7b8543fa
Isaac Hier <isaachier@gmail.com>
2018-05-03 08:02:31 -0400

    Override CMake options directly in Travis
    Signed-off-by: Isaac Hier <isaachier@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -24,8 +24,7 @@ function main() {
 
     mkdir -p build
     cd build
-    coverage_option=${COVERAGE:+"-DJAEGERTRACING_COVERAGE=ON"}
-    cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON "${coverage_option}" ..
+    cmake ${CMAKE_OPTIONS} ..
     make -j3 UnitTest
     info "Running tests..."
     ./UnitTest
