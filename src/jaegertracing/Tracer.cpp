3717a11efe3fea9b42706a28c5f167ed7fc667b2
Oleksandr Bukaiev <bukaevav@gmail.com>
2020-02-19 19:32:09 -0700

    Apparently std::exception ctor with a const char* is not standard
    Signed-off-by: Oleksandr Bukaiev <bukaevav@gmail.com>

--- src/jaegertracing/Tracer.cpp
+++ src/jaegertracing/Tracer.cpp
@@ -75,7 +75,7 @@ Tracer::StartSpanWithOptions(string_view operationName,
         const auto& references = result._references;
         if (self && (parent || !references.empty()))
         {
-            throw std::exception("Self reference must be the only reference");
+            throw std::exception(); // Self reference must be the only reference
         }
 
         std::vector<Tag> samplerTags;
